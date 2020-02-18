d228642547dd6d0b55692ed7831b815f2654104b
Reginald McBride-Taylor <reginald@mcbridetaylor.me>
2020-02-18 17:33:50 -0500

    Clearing out master repo so no-one clones  the incomplete version

--- GraphKit/Styles/GanttChartStyle.swift
+++
@@ -1,9 +0,0 @@
-//
-//  GanttChartStyle.swift
-//  GraphKit
-//
-//  Created by Reginald McBride-Taylor on 2/17/20.
-//  Copyright © 2020 Reginald McBride-Taylor. All rights reserved.
-//
-
-import Foundation
